3df58993c6bd28662298f8261974ead4e288a3ac
Josh <64439962+Joshelby@users.noreply.github.com>
2020-10-23 09:56:56 +0100

    Added Catalogue mode

--- generator.py
+++ generator.py
@@ -2,11 +2,14 @@ import random
 import os
 
 welcome_text = "Welcome to Drawing Idea Generator!"
-#print("Current working directory is: "+os.getcwd())
 os.chdir(r"./option_files")
+mode = 'R'
+
 
 def start():
+    global mode
     print(welcome_text)
+    mode = choose_mode()
     find_idea()
     
 
@@ -24,29 +27,54 @@ def find_idea():
             chosen_element = choose_element(chosen_element)
     restart = input("Would you like to choose again? (If so, enter 'Y')")
     if restart.capitalize() == "Y":
-        print(welcome_text)
-        find_idea()
+        start()
     
+
 def choose_element(name, end = False):
-    if end == True:
-        print("Please choose an idea from the below:")
-    else:
-        print("Please choose a category from the below:")
     options_file = open(name, "r")
     options = options_file.read().split(",")
     options_file.close()
 
     random_elements = []
-    number_of_options = int(min(max(3, len(options)/3), 8))
+    if mode == 'R':
+        number_of_options = int(min(max(3, len(options)/3), 8))
+    else:
+        number_of_options = int(len(options))
 
     for i in range(number_of_options):
         random_element = options.pop(random.randint(0, len(options) - 1))
         random_elements.append(random_element)
         print(str(i) + ": " + random_element.strip("_").capitalize())
     
-    chosen_index = int(input("> "))
-    chosen_element = random_elements[chosen_index]
+    if end == True:
+        print("^^ Please choose an idea from the above ^^")
+    else:
+        print("^^ Please choose a category from the above ^^")
+
+    input_valid = False
+    while input_valid != True:
+        chosen_index = input("> ")
+        if chosen_index.isdigit():
+            if int(chosen_index) <= (len(random_elements) - 1) and int(chosen_index) >= 0:
+                input_valid = True
+                break
+        print("Sorry, that option isn't valid, please try again.")
+    
+    chosen_element = random_elements[int(chosen_index)]
     print("You chose: " + str(chosen_element).strip("_").upper() + ".")
     return chosen_element
+
+
+def choose_mode():
+    input_valid = False
+    while input_valid != True:
+        mode = input("Please choose either Random (R) or Catalogue (C) mode.")
+        try:
+            mode = mode.capitalize()
+            if mode == 'R' or mode == 'C':
+                return mode
+            print("Sorry, that option isn't valid, please try again.")
+        except:
+            print("Sorry, that option isn't valid, please try again.")
 
 start()
